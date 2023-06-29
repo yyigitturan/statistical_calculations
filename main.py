afb80e360e3c05fc573648cbfc4b5df2106764e7
Yaşar Yiğit Turan <125077156+yyigitturan@users.noreply.github.com>
2023-06-30 00:11:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pandas as pd
-import statistics
-import numpy as np
 
-class Statistics:
+class Stat:
     def __init__(self):
         self.data = []
 
+    # Diğer fonksiyonlar buraya eklenecek...
     def get_data_from_csv(self):
         while True:
             csv_file = input("Enter the CSV file name: ")
@@ -39,171 +38,55 @@ class Statistics:
                     self.data.append(num)
                 except ValueError:
                     print("Invalid input. Please try again.")
+    
+
+    def generate_frequency_table(self, sinif_araligi, sinif_sayisi):
+        if not self.data:
+            print("No data available. Please enter data first.")
+            return
+
+        min_deger = min(self.data)
+        max_deger = max(self.data)
+        sinirler = [min_deger]
+        for i in range(1, sinif_sayisi+1):
+            sinirler.append(min_deger + i * sinif_araligi)
+
+        sinif_ortaları = [(sinirler[i] + sinirler[i+1]) / 2 for i in range(len(sinirler)-1)]
+
+        sinif_sikliklari = [0] * sinif_sayisi
+        cumulative_frekans = [0] * sinif_sayisi
+        toplam_siklik = 0
+        for veri in self.data:
+            for i in range(sinif_sayisi):
+                if veri >= sinirler[i] and veri < sinirler[i+1]:
+                    sinif_sikliklari[i] += 1
+                    toplam_siklik += 1
+                    cumulative_frekans[i] = toplam_siklik
+                    break
+
+        sinif_yuzdeleri = [(fi / toplam_siklik) * 100 for fi in sinif_sikliklari]
+
+        tablo = pd.DataFrame({
+            'Alt Sınır': sinirler[:-1],
+            'Üst Sınır': sinirler[1:],
+            'Sınıf Ortaları': sinif_ortaları,
+            'fi': sinif_sikliklari,
+            'Fi': cumulative_frekans,
+            'pi': sinif_yuzdeleri
+        })
+
+        return tablo
 
     def arithmetic_mean(self):
-        if not self.data:
-            print("No data available.")
-            return
+        if self.data == None:
+            print("data yok")
+        else:
+            return sum(self.data) / len(self.data)
 
-        mean = sum(self.data) / len(self.data)
-        print("Arithmetic mean:", mean)
+# Kullanım örneği
 
-    def weighted_mean(self):
-        if not self.data:
-            print("No data available.")
-            return
-
-        weights = []
-        while True:
-            weight = input("Enter a weight for the corresponding number (to finish, enter 'q'): ")
-            if weight.lower() == 'q':
-                break
-            try:
-                weight = float(weight)
-                weights.append(weight)
-            except ValueError:
-                print("Invalid input. Please try again.")
-
-        if len(self.data) != len(weights):
-            print("Number of values and weights should be the same.")
-            return
-
-        weighted_sum = sum([num * weight for num, weight in zip(self.data, weights)])
-        total_weight = sum(weights)
-        weighted_mean = weighted_sum / total_weight
-
-        print("Weighted mean:", weighted_mean)
-
-    def calculate_median(self):
-        if not self.data:
-            print("No data available.")
-            return
-
-        median = statistics.median(self.data)
-        print("Median:", median)
-
-    def calculate_percentile(self):
-        if not self.data:
-            print("No data available.")
-            return
-
-        while True:
-            try:
-                p = float(input("Enter the percentile value (0-100): "))
-                if 0 <= p <= 100:
-                    break
-                else:
-                    print("Invalid percentile value. Please enter a value between 0 and 100.")
-            except ValueError:
-                print("Invalid input. Please enter a numeric value.")
-
-        percentile = np.percentile(self.data, p)
-        print(f"{p}th percentile:", percentile)
-
-    def weighted_median(self):
-        if not self.data:
-            print("No data available.")
-            return
-
-        weights = []
-        while True:
-            weight = input("Enter a weight for the corresponding number (to finish, enter 'q'): ")
-            if weight.lower() == 'q':
-                break
-            try:
-                weight = float(weight)
-                weights.append(weight)
-            except ValueError:
-                print("Invalid input. Please try again.")
-
-        if len(self.data) != len(weights):
-            print("Number of values and weights should be the same.")
-            return
-
-        sorted_indices = np.argsort(self.data)
-        sorted_data = np.array(self.data)[sorted_indices]
-        sorted_weights = np.array(weights)[sorted_indices
-        cumulative_weights = np.cumsum(sorted_weights)
-        total_weight = np.sum(sorted_weights)
-        midpoint = total_weight / 2.0
-
-        if np.all(cumulative_weights <= midpoint):
-            return sorted_data[-1]
-
-        index = np.searchsorted(cumulative_weights, midpoint, side='right')
-        if cumulative_weights[index - 1] == midpoint:
-            return (sorted_data[index - 1] + sorted_data[index]) / 2.0
-        return print(sorted_data[index])
-
-    def trimmed_mean(self, trim_percentage):
-        if not self.data:
-            print("No data available.")
-            return
-
-        data_sorted = sorted(self.data)
-        trim_size = int(len(data_sorted) * trim_percentage / 100)
-        trimmed_data = data_sorted[trim_size:-trim_size]
-
-        if len(trimmed_data) == 0:
-            print("No data remaining after trimming.")
-            return
-
-        mean = sum(trimmed_data) / len(trimmed_data)
-        print("Trimmed Mean: {:.2f}".format(mean))
-
-    def robust_statistics(self):
-        if not self.data:
-            print("No data available.")
-            return
-
-        median = statistics.median(self.data)
-        mad = statistics.median([abs(num - median) for num in self.data])
-        print("Median:", median)
-        print("Median Absolute Deviation (MAD):", mad)
-
-    def perform_operations(self):
-        while True:
-            print("\nOperations Menu:")
-            print("1. Enter data manually")
-            print("2. Import data from CSV")
-            print("-------------------------")
-            print("3. Calculate arithmetic mean")
-            print("4. Calculate weighted mean")
-            print("5. Calculate median")
-            print("6. Calculate percentile")
-            print("7. Calculate weighted median")
-            print("8. Calculate trimmed mean")
-            print("9. Calculate robust statistics")
-            print("100. Exit")
-
-            choice = input("Enter your choice: ")
-            if choice == '1':
-                self.get_data_manually()
-            elif choice == '2':
-                self.get_data_from_csv()
-            elif choice == '3':
-                self.arithmetic_mean()
-            elif choice == '4':
-                self.weighted_mean()
-            elif choice == '5':
-                self.calculate_median()
-            elif choice == '6':
-                self.calculate_percentile()
-            elif choice == '7':
-                self.weighted_median()
-            elif choice == '8':
-                trim_percentage = float(input("Enter the trim percentage (0-100): "))
-                self.trimmed_mean(trim_percentage)
-            elif choice == '9':
-                self.robust_statistics()
-            elif choice == '100':
-                print("Exiting the program...")
-                break
-            else:
-                print("Invalid choice. Please try again.")
-
-
-# Statistics sınıfını kullanarak işlemleri gerçekleştirme
-s = Statistics()
-s.perform_operations()
-
+stat = Stat()
+stat.get_data_from_csv()  # CSV dosyasından veri almak için
+frequency_table = stat.generate_frequency_table(5, 5)
+print(frequency_table)
+print(stat.arithmetic_mean())
